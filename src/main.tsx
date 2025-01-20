01317eba34aee2b3ca8e57b0133ddfc30a9a26c2
EricMI <e.morales@dappertechnologies.com>
2025-01-19 19:17:13 -0600

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import Router from './routes'
 
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <Router />
-  </StrictMode>,
 )
